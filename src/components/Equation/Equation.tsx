d5f9eae59586e7e0d44b771d6e6360708a9fe6da
absolutholz <absolutholz@gmail.com>
2021-07-07 19:03:05 +0200

    Add correct and incorect notifications

--- src/components/Equation/Equation.tsx
+++ src/components/Equation/Equation.tsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import { debounce } from "lodash";
+import React, { useEffect, useState, useRef } from 'react';
+// import { debounce } from "lodash";
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
+type ButtonEvent = React.MouseEvent<HTMLButtonElement>;
 
 enum Operation {
 	ADDITION = 1,
 	SUBTRACTION = 0,
 }
 
+enum State {
+	NOT_STARTED = 0,
+	WAITING = 1,
+	INCORRECT = 2,
+	CORRECT = 3,
+}
+
 const max = 20;
 
 export const Equation = () => {
+	const [ state, setState ] = useState(State.NOT_STARTED)
 	const [ operation, setOperation ] = useState(Operation.ADDITION);
 	const [ first, setFirst ] = useState(0);
 	const [ second, setSecond ] = useState(0);
 	const [ result, setResult ] = useState(0);
+	const formRef = useRef<HTMLFormElement>(null);
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	function createEquation (): void {
 		const operation = Math.round(Math.random());
@@ -30,35 +41,64 @@ export const Equation = () => {
 
 	function onInput (event: InputEvent): void {
 		const solution = parseInt(event.target.value);
+		console.log({solution, result, first, second});
 		if (solution === result) {
+			setState(State.CORRECT);
 			console.log('good job');
 		} else {
+			setState(State.INCORRECT);
 			console.log('try again');
 		}
 	}
 
-	const debounceOnInput = useMemo(() => debounce(onInput, 1000), []);
+	function onNext (event: ButtonEvent): void {
+		createEquation();
+		formRef.current?.reset();
+		inputRef.current?.focus();
+	}
+
+	function onTryAgain (event: ButtonEvent): void {
+	}
 
 	useEffect(() => {
 		createEquation();
 
-		return () => {
-			debounceOnInput.cancel();
-		}
+		// return () => {
+		// 	debounceOnInput.cancel();
+		// }
 	}, []);
 
+	useEffect(() => {
+		setState(State.WAITING);
+	}, [ result ]);
+
 	return (
-		<form>
+		<form ref={ formRef }>
 			<div>
 				{ first } { operation === Operation.ADDITION ? '+' : '-' } { second }
 			</div>
 			<div>
 				= <input
 					inputMode="numeric"
-					onChange={ debounceOnInput }
+					onChange={ onInput }
+					ref={ inputRef }
 					type="number"
 				/>
 			</div>
+
+			{ state === State.CORRECT &&
+				<div>
+					Correct!
+					<button onClick={ onNext } type="button">Next</button>
+				</div>
+			}
+
+			{ state === State.INCORRECT &&
+				<div>
+					Incorrect!
+					<button onClick={ onTryAgain } type="button">Try again</button>
+				</div>
+			}
 		</form>
 	);
 }

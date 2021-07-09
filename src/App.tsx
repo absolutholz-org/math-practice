c9d64a922873adc0b284fe3de38abf66d9afd044
absolutholz <absolutholz@gmail.com>
2021-07-09 22:58:31 +0200

    Add styles to equation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
-import { Equation } from './components/Equation/Equation';
+import "./App.css";
+
+import { Equation } from "./components/Equation/Equation";
+import { Timer } from "./components/Timer/Timer";
 
 function App() {
+	function onTimeOver() {
+		console.log("Time is over");
+	}
+
 	return (
 		<div className="App">
 			<Equation />
+			<Timer seconds={2 * 60} onTimeOver={onTimeOver} />
 		</div>
 	);
 }
--- src/components/Equation/Equation.tsx
+++ src/components/Equation/Equation.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react';
-// import { debounce } from "lodash";
+import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
 type ButtonEvent = React.MouseEvent<HTMLButtonElement>;
@@ -16,22 +16,43 @@ enum State {
 	CORRECT = 3,
 }
 
+const StyledEquation = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	font-size: 4rem;
+	justify-content: center;
+`;
+
+const StyledInput = styled.input`
+	background: #efefef;
+	border: 0;
+	font: inherit;
+	line-height: 1;
+	padding: 0 0 0.1em;
+	text-align: center;
+	width: 3ch;
+`;
+
 const max = 20;
 
 export const Equation = () => {
-	const [ state, setState ] = useState(State.NOT_STARTED)
-	const [ operation, setOperation ] = useState(Operation.ADDITION);
-	const [ first, setFirst ] = useState(0);
-	const [ second, setSecond ] = useState(0);
-	const [ result, setResult ] = useState(0);
+	const [state, setState] = useState(State.NOT_STARTED);
+	const [operation, setOperation] = useState(Operation.ADDITION);
+	const [first, setFirst] = useState(0);
+	const [second, setSecond] = useState(0);
+	const [result, setResult] = useState(0);
 	const formRef = useRef<HTMLFormElement>(null);
 	const inputRef = useRef<HTMLInputElement>(null);
 
-	function createEquation (): void {
+	function createEquation(): void {
 		const operation = Math.round(Math.random());
 		const first = Math.round(Math.random() * max);
-		const second = operation === Operation.ADDITION ? Math.round(Math.random() * (max - first)): Math.round(Math.random() * first);
-		const result = operation === Operation.ADDITION ? first + second : first - second;
+		const second =
+			operation === Operation.ADDITION
+				? Math.round(Math.random() * (max - first))
+				: Math.round(Math.random() * first);
+		const result =
+			operation === Operation.ADDITION ? first + second : first - second;
 
 		setOperation(operation);
 		setFirst(first);
@@ -39,26 +60,25 @@ export const Equation = () => {
 		setResult(result);
 	}
 
-	function onInput (event: InputEvent): void {
+	function onInput(event: InputEvent): void {
 		const solution = parseInt(event.target.value);
-		console.log({solution, result, first, second});
+		console.log({ solution, result, first, second });
 		if (solution === result) {
 			setState(State.CORRECT);
-			console.log('good job');
+			console.log("good job");
 		} else {
 			setState(State.INCORRECT);
-			console.log('try again');
+			console.log("try again");
 		}
 	}
 
-	function onNext (event: ButtonEvent): void {
+	function onNext(event: ButtonEvent): void {
 		createEquation();
 		formRef.current?.reset();
 		inputRef.current?.focus();
 	}
 
-	function onTryAgain (event: ButtonEvent): void {
-	}
+	function onTryAgain(event: ButtonEvent): void {}
 
 	useEffect(() => {
 		createEquation();
@@ -70,35 +90,40 @@ export const Equation = () => {
 
 	useEffect(() => {
 		setState(State.WAITING);
-	}, [ result ]);
+	}, [result]);
 
 	return (
-		<form ref={ formRef }>
-			<div>
-				{ first } { operation === Operation.ADDITION ? '+' : '-' } { second }
-			</div>
-			<div>
-				= <input
+		<form ref={formRef}>
+			<StyledEquation>
+				<span>{first}</span>
+				<span>{operation === Operation.ADDITION ? " + " : " - "}</span>
+				<span>{second}</span>
+				<span> = </span>
+				<StyledInput
 					inputMode="numeric"
-					onChange={ onInput }
-					ref={ inputRef }
+					onChange={onInput}
+					ref={inputRef}
 					type="number"
 				/>
-			</div>
+			</StyledEquation>
 
-			{ state === State.CORRECT &&
+			{state === State.CORRECT && (
 				<div>
 					Correct!
-					<button onClick={ onNext } type="button">Next</button>
+					<button onClick={onNext} type="button">
+						Next
+					</button>
 				</div>
-			}
+			)}
 
-			{ state === State.INCORRECT &&
+			{state === State.INCORRECT && (
 				<div>
 					Incorrect!
-					<button onClick={ onTryAgain } type="button">Try again</button>
+					<button onClick={onTryAgain} type="button">
+						Try again
+					</button>
 				</div>
-			}
+			)}
 		</form>
 	);
-}
+};

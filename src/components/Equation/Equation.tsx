c1911863118826d19685a3870e5728aae499102a
absolutholz <absolutholz@gmail.com>
2021-07-10 12:22:19 +0200

    Update styles of equation window

--- src/components/Equation/Equation.tsx
+++ src/components/Equation/Equation.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 
 import { ReactComponent as SvgCheckMark } from "@mdi/svg/svg/checkbox-marked-circle-outline.svg";
+import { ReactComponent as SvgNext } from "@mdi/svg/svg/arrow-right-thick.svg";
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
 type ButtonEvent = React.MouseEvent<HTMLButtonElement>;
@@ -34,18 +35,69 @@ const StyledInput = styled.input`
 	font: inherit;
 	line-height: 1;
 	outline: none;
-	padding: 0 0 0.1em;
+	padding: 0;
 	text-align: center;
 	width: 3ch;
 `;
 
 const StyledSvgCheckMark = styled(SvgCheckMark)`
-	font-size: 1.25rem;
+	fill: forestgreen;
+	font-size: 5rem;
 	height: 1em;
 	width: 1em;
 `;
 
-const StyledEquationCorrect = styled.div``;
+const StyledEquationCorrect = styled.div`
+	align-items: center;
+	background: hsl(0deg 0% 0% / 25%);
+	bottom: 0;
+	display: flex;
+	justify-content: center;
+	left: 0;
+	position: fixed;
+	right: 0;
+	top: 0;
+`;
+
+const StyledEquationCorrectWindow = styled.section`
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
+		0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
+	padding: 2rem;
+	text-align: center;
+	width: min(20rem, calc(100% - 2rem));
+`;
+
+const StyledEquationCorrectWindowHdln = styled.section`
+	font-size: 2.5rem;
+	font-weight: 100;
+	margin: 0;
+`;
+
+const StyledEquationCorrectWindowFooter = styled.footer`
+	margin-top: 2rem;
+`;
+
+const StyledButton = styled.button`
+	align-items: center;
+	background: rebeccapurple;
+	border: 0;
+	border-radius: 4px;
+	color: #fff;
+	display: inline-flex;
+	font: inherit;
+	margin: 0;
+	padding: 0.5rem 1.5rem 0.6rem;
+`;
+
+const StyledSvgNext = styled(SvgNext)`
+	fill: currentColor;
+	font-size: 1.25em;
+	height: 1em;
+	margin: 0.1em -0.5rem 0 0.5rem;
+	width: 1em;
+`;
 
 const max = 20;
 
@@ -131,11 +183,22 @@ export const Equation = () => {
 
 			{state === State.CORRECT && (
 				<StyledEquationCorrect>
-					<StyledSvgCheckMark />
-					Correct!
-					<button onClick={onNext} type="button">
-						Next
-					</button>
+					<StyledEquationCorrectWindow>
+						<StyledSvgCheckMark />
+						<StyledEquationCorrectWindowHdln>
+							Correct!
+						</StyledEquationCorrectWindowHdln>
+						<div>
+							{first}{" "}
+							{operation === Operation.ADDITION ? " + " : " - "}{" "}
+							{second} = {result}
+						</div>
+						<StyledEquationCorrectWindowFooter>
+							<StyledButton onClick={onNext} type="submit">
+								<span>Next</span> <StyledSvgNext />
+							</StyledButton>
+						</StyledEquationCorrectWindowFooter>
+					</StyledEquationCorrectWindow>
 				</StyledEquationCorrect>
 			)}
 
